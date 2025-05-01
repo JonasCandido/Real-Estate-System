568531b04eff1103c7f65f15fea0eaf2373310e8
JonasCandido <jonasdcandido210@gmail.com>
2025-05-01 20:42:30 -0300

    Add spring boot files

---
+++ real-estate-owners/src/main/java/br/com/jonas/real_estate_owners/RealEstateOwnersApplication.java
@@ -0,0 +1,13 @@
+package br.com.jonas.real_estate_owners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateOwnersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstateOwnersApplication.class, args);
+	}
+
+}
---
+++ real-estate-owners/src/test/java/br/com/jonas/real_estate_owners/RealEstateOwnersApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jonas.real_estate_owners;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateOwnersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
